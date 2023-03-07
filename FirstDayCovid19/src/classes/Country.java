2f219682913f346ac7407861c60ac15e1e4f9488
MigueMat4 <miguemat4@gmail.com>
2023-03-07 08:59:35 -0600

    Estructura base del proyecto

---
+++ FirstDayCovid19/src/classes/Country.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties; // para seleccionar algunas propiedades del JSON
+import org.codehaus.jackson.annotate.JsonProperty; // para hacer match con las propiedades elegidas
+
+/**
+ *
+ * @author Miguel Matul <https://github.com/MigueMat4>
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Country {
+    @JsonProperty("Country")
+    private String country;
+    @JsonProperty("CountryCode")
+    private String country_code;
+    @JsonProperty("Cases")
+    private int cases;
+        
+    public Country() {
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return country_code;
+    }
+
+    public void setCountryCode(String CountryCode) {
+        this.country_code = CountryCode;
+    }
+
+    public int getCases() {
+        return cases;
+    }
+
+    public void setCases(int cases) {
+        this.cases = cases;
+    }
+        
+    @Override
+    public String toString() {
+        return String.valueOf(cases) + " en " + country;
+    }
+}
---
+++ FirstDayCovid19/src/classes/DataTracker.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+/**
+ *
+ * @author migu_
+ */
+public class DataTracker {
+    private static String COVID_API_URL = "https://api.covid19api.com/dayone/country/guatemala/status/confirmed";
+    private List<Country> datos;
+
+    public DataTracker(String pais_a_buscar){
+        COVID_API_URL = "https://api.covid19api.com/dayone/country/"+pais_a_buscar+"/status/confirmed";
+    }
+
+    public void descargarDatos() throws IOException, InterruptedException{
+        System.out.println("Conectando a la API...");
+        // código para conectarse a la API y descargar los datos.
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("Accept", "application/json")
+                .uri(URI.create(COVID_API_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("¡Conexión exitosa! Descargando datos...");
+        ObjectMapper mapper = new ObjectMapper();
+        // obtener listado de datos del país
+        datos = mapper.readValue(response.body(), new TypeReference<List<Country>>() {});
+        System.out.println("¡Datos descargados!");
+    }
+
+    public List<Country> getCountries() {
+        return datos;
+    }
+}
